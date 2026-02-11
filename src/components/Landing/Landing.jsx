d1c8914721b1e43494a77a83b9924a249127c90a
Ami <ami.dorsey@me.com>
2026-02-10 23:31:05 -0500

    App.css file

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router'
-import logo from'../../assets/vybeReazen_logo transparent.png';
+import logo from '../../assets/vybeReazen_logo.png';
 
 const Landing = () => {
   return (
