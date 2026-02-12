f1be039f2d89e192b9ba4f784f457dbfc830755f
Ami <ami.dorsey@me.com>
2026-02-11 22:14:12 -0500

    frequencyService file

---
+++ src/services/frequencyService.js
@@ -0,0 +1,13 @@
+const BASE_URL = `${import.meta.env. VITE_BACK_END_SERVER_URL}`;
+
+const getFrequencies = async() => {
+    const res = await fetch(`${BASE_URL}/frequencies`);
+    return res.json();
+};
+
+const getFrequency = async (hz) => {
+    const res =await fetch(`${BASE_URL}/frequencies/${hz}`);
+    return res.json();
+};
+
+export { getFrequencies, getFrequency};
